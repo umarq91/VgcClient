c4d4a5406f71389ccf8f27345fb1b7b85c4dcbbf
Umer qureshi <Umar Qureshi>
2023-09-14 11:08:17 +0500

    email change

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import Image from 'next/image'
 function Aboutus() {
   return (
-    <div>About Us Page <br/>
+    <div className='mt-4'>
+      
+      
+      
+      
+      <div className='bg-gray-300 min-h-[300px] relative'>
+    <Image fill={true} className='object-cover' src='https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80' />
+      <div className='min-h-[80px] w-1/2 min-w-1/3 bg-green-400 absolute bottom-10 lg:min-w-1/3' >
+        <h1 className='text-black text-3xl font-poppins  pl-6 font-bold items-center'>About Us </h1>
+        <p className='text-black text-sm leading-3 font-poppins font-medium pl-6 pb-4'> About Our Serives , people who work here and how everything works  </p>
+      </div>
+      </div>
+
+
+      
+      About Us Page <br/>
         WHAT DO WE DO<br/>
         WHAT ARE OUR SERVICES<br/>
         CEO INFO <br/>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -16,20 +16,46 @@
 
 // export default WhatsAppButton;
 
+  
+// Phone Number 
+
+// import React from 'react';
+
+// const PhoneCallButton = () => {
+//   const phoneNumber = '923329151242'; // Replace with the phone number you want to call
+
+//   const handleClick = () => {
+//     const phoneURL = `tel:${phoneNumber}`;
+//     window.location.href = phoneURL;
+//   };
+
+//   return (
+//     <button onClick={handleClick}>Call Phone Number</button>
+//   );
+// };
+
+// export default PhoneCallButton;
+
+
+// Email
+
+
 
 import React from 'react';
 
-const PhoneCallButton = () => {
-  const phoneNumber = '923329151242'; // Replace with the phone number you want to call
+const EmailButton = () => {
+  const emailAddress = 'm.umarqureshi091@gmail.com'; // Replace with the email address you want to use
+  const subject = 'Regarding Your Service'; // Replace with the subject of the email
+  const body = 'Hello, I have a question about your service.'; // Replace with the body of the email
 
   const handleClick = () => {
-    const phoneURL = `tel:${phoneNumber}`;
-    window.location.href = phoneURL;
+    const emailURL = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = emailURL;
   };
 
   return (
-    <button onClick={handleClick}>Call Phone Number</button>
+    <button onClick={handleClick}>Send Email</button>
   );
 };
 
-export default PhoneCallButton;
+export default EmailButton;
