894e982e2fd6f3b70e1545edcacb54c1cd324167
noobDevUmar <m.umarqureshi091@gmail.com>
2023-10-02 03:52:17 -0700

    shu

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,8 +13,8 @@ const Projects = () => {
       {PlacesData && PlacesData.length > 0
 
 && PlacesData.map((product) => (
-  
-  <div className='bg-blue-500 h-[65vh] w-96  relative hover:bg-red-500 cursor-pointer  shadow-md rounded-3xl overflow-hidden transition ease-in-out hover:-translate-y-3'>
+
+  <div key={product.title} className='bg-blue-500 h-[65vh] w-96  relative hover:bg-red-500 cursor-pointer  shadow-md rounded-3xl overflow-hidden transition ease-in-out hover:-translate-y-3'>
     <Image src={product.img} fill={true}  className='object-cover opacity-40'/>
   <h3 className='text-white absolute bottom-12 left-4'> {product.type} </h3>
   <h3 className='text-white text-2xl absolute bottom-5 left-4 font-semibold '>{product.title} </h3>
