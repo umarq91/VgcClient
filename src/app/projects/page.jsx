86eac4720b9fe6628adc679551fd0c4ab2ed6645
noobDevUmar <m.umarqureshi091@gmail.com>
2023-10-05 03:13:45 -0700

    sho

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -7,7 +7,7 @@ import img2 from "@/public/project2.avif";
 
 function Projects() {
   return (
-    <div className="container mx-auto max-w-screen-lg">
+    <div className="">
      <div className=" h-[400px] xl:h-[500px] md:h-[450px] w-full relative">
   <div
     className="absolute inset-0"
@@ -17,18 +17,20 @@ function Projects() {
       backgroundSize: "auto",
     }}
   ></div>
+
   <div className="absolute inset-0 bg-black opacity-50" style={{ filter: "brightness(0.5)" }}></div>
   <div className="flex flex-col items-center justify-center h-full relative">
     <h1 className="text-white m-0 text-5xl font-bold tracking-[9px]">PROJECTS</h1>
     <h2 className="text-center text-white"> By VISION GROUP OF COMPANIES</h2>
   </div>
 </div>
+
 <h1 className="font-light text-3xl md:text-4xl lg:text-5xl xl:text-5xl mt-16 text-center mt-4 text-gray-700 tracking-[2px] underline decoration-orange-500/80 ">
         {" "}
        ON GOING PROJECTS {" "}
       </h1>
 
-      <div className="grid gap-6 mt-8 lg:grid-cols-2  md:grid-cols-2 grid-cols-1 xl:grid-cols-3 items-center place-items-center  ">
+      <div className="grid gap-6 mt-8 lg:grid-cols-2  md:grid-cols-2 grid-cols-1 xl:grid-cols-3 items-center place-items-center container mx-auto  max-w-screen-lg ">
         <ProjectCard img={img1} title="Darmangi" type="Residential" location="Peshawar"/>
         <ProjectCard img={img2} title="Wisal-garden" type="Commercial" location="Peshawar"/>
         <ProjectCard img={img1} title="Darmangi" type="Residential" location="Peshawar"/>
