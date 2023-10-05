18e78377f5e5ab7a6950b3e8b55477920347da38
noobDevUmar <m.umarqureshi091@gmail.com>
2023-10-05 11:28:27 -0700

    shuuuu

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from 'next/image'
+import { useState } from 'react'
 import Hero from '../components/Hero'
 import Stats from '../components/Stats'
 import AboutUs from '../components/AboutUs';
@@ -12,21 +13,28 @@ import img1 from "@/public/project1.avif";
 import img2 from "@/public/project2.avif";
 import video from "@/public/intro.mp4"
 export default function Home() {
-  const latitude = 34.0329245;
-  const longitude = 71.5983885;
-  const zoomLevel = 100;
 
-  const mapSrc = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3305.330685110177!2d${longitude}!3d${latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38dfb95d84aaae21%3A0x166e06299e5a5f7f!2s${latitude}%2C%20${longitude}!5e0!3m2!1sen!2sen!4v1633280623754!5m2!1sen!2sen&z=${zoomLevel}`;
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const handleVideoPlay = () => {
+    setIsVideoPlaying(true);
+  };
+  
   return (
     <div className="test max-w-[100vw]">
+
 <div className="relative h-[100vh]">
-  <video
+  {!isVideoPlaying ? (
+    
+    //  Video and layout div 
+    <>
+    <video
     autoPlay
     muted
+    onPlay={()=>setIsVideoPlaying(true)}
     loop
     className="h-[100vh] w-full object-cover -z-1"
     style={{ filter: "brightness(50%)" }} // Adjust the brightness value as needed
-  >
+    >
     <source src={video} type="video/mp4" />
   </video>
   <div className="absolute inset-0 flex items-center justify-center">
@@ -35,6 +43,12 @@ export default function Home() {
       <p className="text-2xl  drop-shadow-lg">Private Limited</p>
     </div>
   </div>
+    </>
+)  : (<div className='text-center flex justify-center'> 
+  <h1> Loading Video ...</h1>
+
+</div>) }
+
 </div>
 
 
@@ -45,15 +59,7 @@ export default function Home() {
         <Hero />
       </div>
 
-      <iframe
-        src={mapSrc}
-        width="100%"
-        height="500px"
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-        title="Google Map"
-      ></iframe>
+      
 
       {/* <div className="mt-24 mb-24"> <Stats /></div> */}
       {/* <hr className=" text-black mt-10 opacity-70 innerWidth" /> */}
