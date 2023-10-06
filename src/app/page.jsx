4ce0dabd89a8845587c5e539e4e9be46748f1c6c
noobDevUmar <m.umarqureshi091@gmail.com>
2023-10-05 23:57:19 -0700

    6oct

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,86 +1,21 @@
-"use client"
-import Image from 'next/image'
-import { useState } from 'react'
-import Hero from '../components/Hero'
-import Stats from '../components/Stats'
+"use client";
+import React from 'react';
+import LandingPagevideo from '@/components/LandingPagevideo';
 import AboutUs from '../components/AboutUs';
-import Projects from '@/components/Projects';
-import { Carousel } from 'react-responsive-carousel';
-import Slider from '@/components/Slider';
-import ContactUs from '@/components/contactus';
 import ProjectCard from '@/components/ProjectCard';
 import img1 from "@/public/project1.avif";
 import img2 from "@/public/project2.avif";
-import video from "@/public/intro.mp4"
-export default function Home() {
+import AboutUsNew from '@/components/AboutUsNew';
 
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
-  const handleVideoPlay = () => {
-    setIsVideoPlaying(true);
-  };
-  
+export default function Home() {
   return (
     <div className="test max-w-[100vw]">
+      <LandingPagevideo />
 
-<div className="relative h-[100vh]">
-  {!isVideoPlaying ? (
-    
-    //  Video and layout div 
-    <>
-    <video
-    autoPlay
-    muted
-    onPlay={()=>setIsVideoPlaying(true)}
-    loop
-    className="h-[100vh] w-full object-cover -z-1"
-    style={{ filter: "brightness(50%)" }} // Adjust the brightness value as needed
-    >
-    <source src={video} type="video/mp4" />
-  </video>
-  <div className="absolute inset-0 flex items-center justify-center">
-    <div className="text-white text-center">
-      <h1 className="text-6xl font-bold font-serif drop-shadow-lg">Vision Group Of <span className='text-red-800 underline'>Companies </span> </h1>
-      <p className="text-2xl  drop-shadow-lg">Private Limited</p>
-    </div>
-  </div>
-    </>
-)  : (<div className='text-center h-[100vh] flex justify-center items-center'> 
-<div role="status">
-    <svg aria-hidden="true" class="w-12 h-12 ml-6 text-gray-200 animate-spin text-center dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>
-    <span class="sr-only">Loading...</span>
-    <h1> Loading Video</h1>
-</div>
-
-</div>) }
-
-</div>
+      <AboutUsNew/>
 
 
-     
-
-      <div className="mt-6">
-        {" "}
-        <Hero />
-      </div>
-
-      
-
-      {/* <div className="mt-24 mb-24"> <Stats /></div> */}
-      {/* <hr className=" text-black mt-10 opacity-70 innerWidth" /> */}
-
-      <AboutUs />
-      {/* <AboutusNew/> */}
-
-      {/* <hr className="pt-20 text-black mt-10 opacity-70 innerWidth" /> */}
-      {/* <hr className=" lg:pb-20 opacity-70 innerWidth" /> */}
-
-      {/* <hr className=" lg:pb-20 opacity-70 innerWidth" /> */}
-      {/* <h1 className='flex justify-center text-4xl mt-4 font-bold mx-auto sm:text-5xl lg:text-6xl'> Discover Our Projects </h1>
-      <div className="mt-20"> <Slider /></div> */}
-
+    <div> <h1 className='text-3xl font-lora '>Featured Projects  </h1> </div>
       <div className="grid gap-6 mt-20 lg:grid-cols-2 md:grid-cols-2 grid-cols-1 xl:grid-cols-3 items-center place-items-center ">
         <ProjectCard
           img={img1}
@@ -103,9 +38,6 @@ export default function Home() {
       </div>
 
       <hr className="pt-20 mt-10 opacity-70 innerWidth" />
-      <hr className=" lg:pb-20 opacity-70 innerWidth" />
-
-      {/* <ContactUs/> */}
     </div>
   );
 }
---
+++ src/components/AboutUsNew.jsx
@@ -0,0 +1,76 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import ceo from "@/public/sahil.JPG"
+const AboutUsNew = () => {
+    const content = {
+        content:"We are an emerging group of various businesses like real estate development, construction services and planning, corporate brand building, Investment planning, sales & marketing services, and digital services. The group leaders aim to make the best real estate of Pakistan by the integration of modern technology, hardworking team and unique ideas. Thus, the (Vision Group of Companies) will act as the leading smartest Real Estate Company and play its due role in Pakistan's real estate development. The corporations head office is in Peshawar, with a plethora of site offices spreading across the country. Our vision is to become Pakistan's first value-based organization and revolutionize the real estate industry in Pakistan with 100% investor confidence, satisfaction and country-wide projects"
+        ,services:"At our marketing agency specializing in real estate, we offer a comprehensive suite of services tailored to meet the evolving needs of our clients in today's dynamic market"
+      , vision:"Our vision is to surpass all expectations and become the foremost real estate company in Peshawar by harnessing the power of integrated technology, delivering an exceptional, seamless experience to our valued customers, and leading the industry with innovation and excellence.. Our expertise spans a wide spectrum of offerings, including:"
+      ,ceoinfo:"Shahabul-Islam Sahil is the visionary leader at the helm of an organization, responsible for shaping its overall strategic direction and ensuring its successful execution. As the highest-ranking executive, He plays a pivotal role in driving the company's mission, growth, and profitability. Sahil, our esteemed CEO, is a visionary leader who has been instrumental in guiding VISION MARKETING GROUP of COMPANIES to new heights of success. With a distinguished career spanning 5+ years in Peshawar, Sahil has consistently demonstrated a remarkable ability to steer our company towards excellence."
+      ,mdinfo:"M. Tayyab Khan: Leading the Way in Real Estate and Marketing . In the dynamic world of real estate and marketing, leadership plays a pivotal role in shaping the success of a company. At the helm of your organization stands Mr. M. Tayyab Khan, a visionary leader whose dedication and expertise have propelled the company to new heights."
+      ,managerinfo:"Meet Shahnawaz, an exceptional marketing and real estate manager whose expertise and passion have made him a true leader in the industry. With a track record of success that spans several years, Shahnawaz has proven time and again that he is a force to be reckoned with in the world of real estate and marketing."
+      }
+  return (
+    <div className='bg-gray-200 shadow-lg drop-shadow-2xl'>
+        
+      {/* About Us */}
+      <h1 className="font-light text-5xl text-center mt-4 text-gray-700 tracking-[2px] underline decoration-red-500 font-lora">
+        {" "}
+       COMPANY INTRODCUTION{" "}
+      </h1>
+
+{/* CEO  */}
+ <div className="px-12 lg:flex md:flex lg:items-center lg:px-16 xl:px-16 xl:mx-auto gap-[30px] mt-10">
+        {/* Image */}
+        <div className="items-center md:ml-10 flex mx-auto md:w-1/2 lg:1/3 justify-center">
+          <Image
+            src={ceo}
+            alt="image"
+            width={300}
+            height={300}
+            objectFit="cover"
+            className="rounded-full drop-shadow-xl object-cover md:rounded-lg lg:rounded-lg xl:rounded-xl"
+          />
+        </div>
+        {/* Content */}
+        <div className="mt-5 mx-5 md:w-1/2 lg:1/2">
+          <h2 className="text-2xl text-orange-600 text-center tracking-[15px]">
+            {" "}
+            CEO{" "}
+          </h2>
+          <h2 className="text-5xl text-gray-800 text-center font-bold font-lora tracking-[2px]">
+            {" "}
+            Shahabul-Islam Sahil{" "}
+          </h2>
+          <p className="sm:text-sm">{content.ceoinfo}</p>
+        </div>
+      </div>
+
+
+
+
+    <div className="px-4 flex flex-col mx-auto w-100  max-w-[80%] tracking-[2px] md:max-w-[60%] lg:max-w-[50%]   items-center    gap-[30px]">
+        {/* Intro */}
+        <div className="">
+          <h1 className="font-bold text-center text-5xl text-orange-700 mt-4 md:text-4xl lg:text-4xl xl:text-5xl mt-[30px]">
+            {" "}
+            Who Are We ?{" "}
+          </h1>
+
+          <p className="text-md md:text-lg ">{content.content} </p>
+        </div>
+    <Link  href='/about-us'>
+      <button className='text-white  text-lg m-2 bg-red-800 hover:bg-gray-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'> Learn More About us </button>
+    </Link>
+      </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default AboutUsNew
