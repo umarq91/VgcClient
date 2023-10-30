f63c034f37065cb26b38aa6eba79e1ed9df306d8
noobDevUmar <m.umarqureshi091@gmail.com>
2023-10-30 09:06:05 -0700

    main

--- src/clientData/PlacesData.jsx
+++ src/clientData/PlacesData.jsx
@@ -3,11 +3,11 @@ import img1  from "@/public/project1.avif"
 import img2 from "@/public/project2.avif"
 
 const  PlacesData=[
-    {title:"Wisal Garden",img:img1,name:"wisal-garden",type:'Residential',location:"Peshawar"},
+    {title:"Wisal Garden",img:img1,name:"wisal-garden",type:'Housing Society',location:"Peshawar"},
     {title:"Vision Enclave",img:img2,name:'vision-enclave',type:'Housing Area'},
-    {title:"Orchard Square",img:'/darmangi.jpg',name:'orchard-square',type:'Residential'},
-    {title:"Wisal Garden",img:'/wisalgarden.jpg',name:'wisal-garden',type:' Mall & Residential'},
-    {title:"Darmangi",img:'/darmangi.jpg',name:'darmangi',type:'Residential'},
+    {title:"Orchard Square",img:'/darmangi.jpg',name:'orchard-square',type:'Housing Society'},
+    {title:"Wisal Garden",img:'/wisalgarden.jpg',name:'wisal-garden',type:' Housing Society'},
+    {title:"Darmangi",img:'/darmangi.jpg',name:'darmangi',type:'Housing Society'},
     {title:"Wisal Garden",img:'/wisalgarden.jpg',name:'wisal-garden',type:'Housing Society'},
 ]
 
